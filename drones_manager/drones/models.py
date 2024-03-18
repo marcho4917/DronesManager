e90e57b32aaff6ec506d89d4f53957dd0967d3f5
Martyna Chojnowska <martynachojnowska12@gmail.com>
2024-03-18 09:52:08 +0100

    Battery range validator added

--- drones_manager/drones/models.py
+++ drones_manager/drones/models.py
@@ -4,10 +4,10 @@ from django.utils import timezone
 
 class Drone(models.Model):
     name = models.CharField(max_length=100)
-    status = models.CharField(max_length=50)
+    status = models.CharField(max_length=50, blank=True)
     battery_level = models.FloatField()
-    current_location = models.CharField(max_length=100)
-    last_updated_time = models.DateTimeField(default=timezone.now)
+    current_location = models.CharField(max_length=100, blank=True)
+    last_updated_time = models.DateTimeField(default=timezone.now, blank=True)
 
     def __str__(self):
         return self.name
