b370c77ea13a9c4660a28fb015880fdc647553a7
Martyna Chojnowska <martynachojnowska12@gmail.com>
2024-03-20 10:45:02 +0100

    Unnecessary code deleted

--- drones_manager/drones/tasks.py
+++ drones_manager/drones/tasks.py
@@ -7,15 +7,14 @@ from faker import Faker
 @shared_task
 def update_drone_data():
     drones = Drone.objects.all()
-
     for drone in drones:
-        status = random.choice(['Active', 'Inactive', 'Service', 'Battery charge'])
-        if status == 'Service':
+        drone.status = random.choice(['Active', 'Inactive', 'Service', 'Battery charge'])
+        if drone.status == 'Service':
             drone.current_location = 'Home'
         else:
             faker = Faker()
             latitude = faker.latitude()
             longitude = faker.longitude()
             drone.current_location = f"{latitude}, {longitude}"
-        drone.battery_level = drone.battery_level = random.randint(0, 101)
+        drone.battery_level = random.randint(0, 101)
         drone.save()
